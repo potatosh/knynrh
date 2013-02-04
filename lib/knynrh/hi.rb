b8bb573403245706db03f10f965e4f706564be9f
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2013-02-04 19:17:24 +0900

    develop
    Chatroid.newに渡してるブロック内のtweetメソッドが呼び出されることをテストしたほうが良い？

--- lib/knynrh/hi.rb
+++ lib/knynrh/hi.rb
@@ -3,5 +3,20 @@ require 'chatroid'
 class Knynrh
   module Hi
 
+    private
+
+    def launch
+      Chatroid.new do
+        set :service, 'Twitter'
+        set :consumer_key, ENV['CONSUMER_KEY']
+        set :consumer_secret, ENV['CONSUMER_SECRET']
+        set :access_key, ENV['ACCESS_KEY']
+        set :access_secret, ENV['ACCESS_SECRET']
+
+        on_time :hour => 18, :min => 0, :sec => 0 do
+          tweet 'hi'
+        end
+      end.run!
+    end
   end
 end
--- spec/knynrh/hi_spec.rb
+++ spec/knynrh/hi_spec.rb
@@ -2,12 +2,13 @@ require 'knynrh'
 
 describe Knynrh::Hi do
   let(:knynrh) do
-    Knynrh.new do
-      extend Knynrh::Hi
-    end
+    Knynrh.new.extend Knynrh::Hi
   end
 
   describe '#launch' do
-    it 'should run new Chatroid'
+    it 'should run new Chatroid' do
+      Chatroid.any_instance.should_receive(:run!)
+      knynrh.send(:launch)
+    end
   end
 end
